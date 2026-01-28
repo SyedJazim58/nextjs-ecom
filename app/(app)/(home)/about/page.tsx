150566f588bf760e5abdced4ca3ea22e4843e593
SyedJazim58 <syedjazim58@gmail.com>
2026-01-28 20:00:21 +0500

    feat: fetch and display categories in about page

--- app/(app)/(home)/about/page.tsx
+++ app/(app)/(home)/about/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { Categories } from '@/payload-types';
+import ConfigPromise from '@payload-config'
+import { getPayload } from 'payload'
 
-const About = () => {
+async function About() {
+  const payload = await getPayload({
+    config: ConfigPromise
+  })
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1,
+    pagination: false,
+    where: {
+      parent: {
+        exists: false
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategory: (doc.subcategory?.docs ?? []).map((doc: Categories) => ({
+      ...(doc as Categories),
+      subcategory: undefined,
+    })), // Extract subcategory docs
+  }));
+
+  console.log({ data, formattedData });
   return (
-    <div>About</div>
+    <div>
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <div>{JSON.stringify(formattedData, null, 2)}</div>
+    </div>
   )
 }
 
